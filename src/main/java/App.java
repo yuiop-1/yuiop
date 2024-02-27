96cae592a9ca8b7dc3fa56c4a2099f93a7b9c5e4
Терминал9 <term9@classroom.local>
2024-02-27 11:51:46 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ввeдите пример: ");
+        String input = scanner.nextLine();
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            System.out.print("Ошибка. Нужны 2 значения и оператор");
+            return;
+        }
+        int num0 = Integer.parseInt(parts[0]);
+        char oper = parts[1].charAt(0);
+        int num1 = Integer.parseInt(parts[2]);
+        int res = 0;
+
+        switch(oper) {
+            case '-':
+                res = num0 - num1;
+                break;
+            case '+':
+                res = num0 + num1;
+                break;
+            case '*':
+                res = num0 * num1;
+                break;
+            case '/':
+                if (num1 != 0) {
+                    res = num0 / num1;
+                } else {
+                    System.out.print("Ошибка. Деление на 0");
+                    return;
+                }
+                break;
+            default:
+                System.out.print("Неверный оператор");
+                return;
+        }
+        System.out.println("ответ:" + res);
+
+    }
+}
